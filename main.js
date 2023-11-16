d2591b91b85961b81c6683b059f813d6fa65b66e
Ana0500 <127546183+Ana0500@users.noreply.github.com>
2023-11-16 14:17:31 -0300

    mains

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;  //template string 
-
+    const idAudio = `#som_${instrumento}`; //template string 
+}
     tecla.onclick = function (){
         tocaSom(idAudio);
     }
